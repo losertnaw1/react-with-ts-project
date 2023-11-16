5ca37e0cf004052322767ab66d076cfb8faa02d9
DatLe <quale.2112@gmail.com>
2023-11-16 17:04:09 +0700

    upgrade by tailwindcss

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,12 +10,14 @@ type Props = {
 
 export function Card(props: Props) {
   return (
-    <div className='card'>
-      <img src={props.src} alt={props.title} />
-      <div className="infor">
-          <strong className="title">{props.title}</strong>
-          <p>Price : <span className="price">{'$' + props.price}</span></p>
-          <button onClick={props.addToCart}>ADD TO CART</button>
+    <div className='group relative'>
+      <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'>
+        <img src={props.src} alt={props.title} className='h-full w-full object-cover object-center lg:h-full lg:w-full'/>
+      </div>
+      <div className="mt-4 flex justify-between">
+        <h3 className="text-sm text-gray-700">{props.title}</h3>
+        <button className='add-to-cart' onClick={props.addToCart}>ADD TO CART</button>
+        <p className='text-sm font-medium text-gray-900'>Price :{'$' + props.price}</p>
       </div>
     </div>
   );
